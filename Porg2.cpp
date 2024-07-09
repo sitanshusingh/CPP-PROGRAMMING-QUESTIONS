5e88394d7d5afb6b615a60036f78acacc8b07900
SitanshuSingh <sitanshusingh45972@gmail.com>
2024-07-09 19:03:26 +0530

    Add New File

---
+++ Porg2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+// Convert Character to ASCII Number
+int main(){
+    char ch;
+    cout<<"Enter Char word "<<endl;
+    cin>>ch;
+    cout<<"Thee ASCII value of Char is "<<int(ch);
+    return 0;
+}
