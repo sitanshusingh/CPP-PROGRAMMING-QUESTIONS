2e7aa2cadb9d5a1ef27ede648bed4cdf31ecc494
Sitanshu Singh <sitanshusingh45972@gmail.com>
2024-10-25 18:56:15 +0530

    Array

---
+++ Array Question/Program54.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*Print all pairs of numbers from the input array whose sum is greater than the Sum value k.*/
+
+int main()
+{
+    int size, k;
+    cout << "Enter a Size of  array: ";
+    cin >> size;
+    int arr[size];
+    cout << "Enter the array elements: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter the Value of K: ";
+    cin >> k;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] + arr[j] > k)
+            {
+                cout << arr[i] << " " << arr[j] << endl;
+            }
+        }
+    }
+
+    return 0;
+}
