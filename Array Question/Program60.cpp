b1e30ef79eb31b906797a0ad8c0b03052e591fb6
Sitanshu Singh <sitanshusingh45972@gmail.com>
2024-10-28 22:05:54 +0530

    Array

---
+++ Array Question/Program60.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*Given an array of integers, count the number of elements which are Lesser then k and Odd*/
+
+int main()
+{
+
+    int arr[] = {10, 5, 20, 7, 12};
+    int size = sizeof(arr) / sizeof(int);
+    int count = 0, k;
+    cout << "Enter the vlaue of k ";
+    cin >> k;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 != 0 && arr[i] < k)
+        {
+            count++;
+        }
+    }
+    cout << "Number of element Lesser then K and Odd Number is " << count;
+    return 0;
+}
