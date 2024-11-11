478f8e724cb537ffdf211be749fa6bdd482bde84
Sitanshu Singh <sitanshusingh45972@gmail.com>
2024-11-11 23:09:50 +0530

    Array

---
+++ IF Question/Program1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Even or Odd
+
+int main(){
+int num;
+cout<<"Enter a number:- ";
+cin>>num;
+if(num%2==0){
+    cout<<num<<" is a Even Number";
+}
+else{
+    cout<<num<<" is a Odd Number ";
+}
+
+return 0;
+}
